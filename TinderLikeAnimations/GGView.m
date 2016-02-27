a5454fa2f4995ce752e028d7adde4e24f28f54cb
Mariah <mflaim1@ithaca.edu>
2016-02-27 16:38:39 -0500

    first commit

---
+++ TinderLikeAnimations/GGView.m
@@ -0,0 +1,31 @@
+#import "GGView.h"
+#import "GGDraggableView.h"
+
+
+@interface GGView ()
+@property(nonatomic, strong) GGDraggableView *draggableView;
+@end
+
+@implementation GGView
+
+- (id)init
+{
+    self = [super init];
+    if (!self) return nil;
+
+    self.backgroundColor = [UIColor whiteColor];
+
+    [self loadDraggableCustomView];
+
+    return self;
+}
+
+- (void)loadDraggableCustomView
+{
+    //change size here
+    self.draggableView = [[GGDraggableView alloc] initWithFrame:CGRectMake(60, 60, 200, 260)];
+
+    [self addSubview:self.draggableView];
+}
+
+@end
