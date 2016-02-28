86c3967bdc317a2659f0c299a337b0657c399868
Shelby Cohen <scohen4@ithaca.edu>
2016-02-28 02:36:33 -0500

    Applicant View Page

---
+++ TinderLikeAnimations/ApplicantController.h
@@ -0,0 +1,15 @@
+//
+//  ApplicantController.h
+//  Resumatch
+//
+//  Created by S Cohen on 2/28/16.
+//  Copyright © 2016 theguti.self. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ApplicantController : UIViewController
+@property (weak, nonatomic) IBOutlet UIButton *uploadResumeButton;
+@property (weak, nonatomic) IBOutlet UIButton *SubmitProfileButton;
+
+@end
